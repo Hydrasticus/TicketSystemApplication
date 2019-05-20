790254c4e21534b3302c4bffe5dedbd716cd0ead
Hydrasticus <alo.pullmann@gmail.com>
2019-05-20 19:31:34 +0300

    Updated controller with PUT and DELETE methods

--- TicketSystemApplication/Controllers/TicketsController.cs
+++ TicketSystemApplication/Controllers/TicketsController.cs
@@ -89,5 +89,56 @@ namespace TicketSystemApplication.Controllers {
 
             return CreatedAtAction("GetTicket", new {id = ticket.Id}, ticket);
         }
+
+        // PUT: api/Tickets/5
+        /// <summary>
+        /// Updates a ticket via its ID. First checks whether it exists.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="ticket"></param>
+        /// <returns></returns>
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTicket(Guid id, Ticket ticket) {
+            if (id != ticket.Id) {
+                return BadRequest();
+            }
+
+            _context.Entry(ticket).State = EntityState.Modified;
+
+            try {
+                await _context.SaveChangesAsync();
+            } catch (DbUpdateConcurrencyException) {
+                if (!TicketExists(id)) {
+                    return NotFound();
+                } else {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Tickets/5
+        /// <summary>
+        /// Deletes a ticket via its ID.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Ticket>> DeleteTicket(Guid id) {
+            var ticket = await _context.Ticket.FindAsync(id);
+            if (ticket == null) {
+                return NotFound();
+            }
+
+            _context.Ticket.Remove(ticket);
+            await _context.SaveChangesAsync();
+
+            return ticket;
+        }
+
+        private bool TicketExists(Guid id) {
+            return _context.Ticket.Any(e => e.Id == id);
+        }
     }
 }
